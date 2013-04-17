f460d1cfaa366f5e18848e13379bcb8670ec8356
dmcalamares <diane_calamares@yahoo.com>
2013-04-17 11:44:46 +0800

    Update Reverse.java

--- src/Reverse.java
+++ src/Reverse.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 public class Reverse {
 	public static void main(String [] args){
 		String input = "";
-		input =JOptionPane.showInputDialog("Enter a word");
+		input =JOptionPane.showInputDialog("Enter words:");
 		int len= input.length();
 		for(int i=len;i!=0;i--){
 		System.out.print(input.charAt(i-1));

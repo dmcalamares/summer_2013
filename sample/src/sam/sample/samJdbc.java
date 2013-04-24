969c01e017ed981201dac4c084a43268e8d73599
dmcalamares <diane_calamares@yahoo.com>
2013-04-24 10:21:33 +0800

    Update samJdbc.java

--- sample/src/sam/sample/samJdbc.java
+++ sample/src/sam/sample/samJdbc.java
@@ -2,7 +2,6 @@ package sam.sample;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-//import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.*;
 
